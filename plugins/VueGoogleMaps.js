1cead53835c3f123509ee4d249953df49966c51f
Jordan-T <jordan.taisne@gmail.com>
2022-08-18 22:37:57 +0200

    chore: allow gmap without key

--- plugins/VueGoogleMaps.js
+++ plugins/VueGoogleMaps.js
@@ -1,12 +1,16 @@
 import VueGoogleMaps from "@fawmi/vue-google-maps";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const config = useRuntimeConfig().public;
+  const runtimeConfig = useRuntimeConfig().public;
+  const config = {
+    language: "fr",
+  };
+
+  if (runtimeConfig.GOOGLE_MAPS_API_KEY) {
+    config.key = runtimeConfig.GOOGLE_MAPS_API_KEY;
+  }
 
   nuxtApp.vueApp.use(VueGoogleMaps, {
-    load: {
-      key: config.GOOGLE_MAPS_API_KEY,
-      language: "fr",
-    },
+    load: config,
   });
 });
